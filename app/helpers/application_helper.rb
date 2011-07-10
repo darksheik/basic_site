f2bfa8b58d4620fac773020f58cbd5c6bff6c1ad
Don Pflaster <dpflaster@gmail.com>
2011-07-10 18:12:25 -0400

    Basic user model site

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,7 @@
+module ApplicationHelper
+  
+  def logo
+    image_tag("kittybase_logo.gif", :alt => "KittyBase Logo", :class => "round")
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittybase::Application.config.secret_token = '9bc553ba5077b0ffed0c908f4c873437111a3e06acc5be7ca5711904ee2cfe372f6dd94751eff85fdbd8ceb4b7652add64f4867b5c2a39b63e2c5140d6b18b24'
