0bd7b51c41df0540d0c9758bd6b6e9e63d75c392
Don Pflaster <dpflaster@gmail.com>
2011-07-10 22:31:50 -0400

    Fixed user controller items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+
   
   def index
     @title = "All users"
@@ -61,6 +63,9 @@ end
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
   
 end
